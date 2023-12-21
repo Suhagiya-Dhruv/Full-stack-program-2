0e470888bdb938ac28a5153cf37aeaab949245e2
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-12-21 14:21:19 +0530

    Revison Loop

---
+++ intro.js
@@ -0,0 +1,73 @@
+
+// var a = 10;
+// // console.log(a++); // post increment
+// // console.log(++a); // pre increment
+
+// // console.log(a >= 10)
+
+// a += 10;  // a = a + 10;
+
+// console.log(a);
+
+
+// var day = 7;
+
+// if(day == 1){
+//     console.log("Monday")
+// }else{
+//     console.log("Not Monday")
+// }
+
+// if(day == 6){
+//     console.log("WeekEnd")
+// }else if(day == 7){
+//     console.log("Week end Sunday")
+// }else{
+//     console.log("Weekday")
+// }
+
+// var day = 5;
+
+// if (day > 5) {
+//     if (day == 6) {
+//         console.log("WeekEnd")
+//     } else {
+//         console.log("Weekend Sunday")
+//     }
+// }
+
+// switch (day) {
+//     case 1: console.log("M");
+//         break;
+//     case 2: console.log("T");
+//         break;
+//     case 5: console.log("F");
+//         break;
+//     default: console.log("S");
+//         break;
+// }
+
+// for
+// while
+// dowhile
+
+// for (var i = 0; i < 5; i = i + 2) {
+//     console.log(i);
+//     i++;
+// }
+
+// [a, b] = b - a + 1;
+//[ 15, 43] = 43-15+1;
+
+// [3, 17] = 17-3+1
+
+// var i = 3;
+// while (i < 3) {
+//     console.log("while",i++);
+// }
+
+// do {
+//     console.log("do while",i++)
+// } while (i < 3);
+
+console.log("hello")
