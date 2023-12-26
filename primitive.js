2c0edb553ae7257a95a47fbd030ce10cdcdd5bd8
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-12-26 16:49:31 +0530

    Primitive Datatype

---
+++ primitive.js
@@ -0,0 +1,67 @@
+// int a = 10; // (Number)
+// char c = 'c';
+// float f = 3.14;
+// double d =  45484564484545;
+// boolean b = true;
+// string c = "string"
+
+// var a = 10; // Number
+// var b = '10'; // String
+// var c = 1.5; // Float
+// var d = true; // Boolean
+// var e = 'c'; // String
+// var f;
+// var g = null;
+// var h = undefined;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+// console.log(typeof(f));
+// console.log(typeof(g));
+// console.log(typeof(h));
+
+
+// var s = "let's go"; // let's go
+
+// var color = "red";
+
+// var s2 = "Apple is "+color+" color";
+// var s3 = `Apple is ${color} color`;
+
+`` // template literals
+
+// console.log(s)
+// console.log(s2)
+// console.log(s3);
+
+var s = "                Hello          My name is              ";
+var name = " John";
+// var s1 = "Hello My name is Bob";
+// console.log(s.split()); // return array
+// console.log(s.split("")); // return array
+// console.log(s.split(" ")); // return array
+// console.log(s.split("a")); // return array
+// console.log(s.split("e")); // return array
+
+// console.log(s.charAt(0)); // index number
+
+// console.log(name.concat(name)); // Join two string
+
+// console.log(name.length)
+
+// console.log(s)
+// console.log(s.trim())
+
+
