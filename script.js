eb2edd942c6ceb93cae1d9fcb8be34b9421b86c1
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2024-01-06 10:46:59 +0530

    This Keyword

--- script.js
+++ script.js
@@ -1,115 +1,53 @@
-// var a = {
-//     name: "hello",
-//     age: 18,
-//     city: "UK",
-//     pincode: "548958"
-// }
+// let a = 15;
 
+// function hello(){
 
-// var b = [1, 2, 3, 4, 5, 6];
-
-
-// console.log(a.name);
-
-// console.log(b[0]);
-// console.log(b[1]);
-// console.log(b[2]);
-// console.log(b[3]);
-// console.log(b[4]);
-
-// for(var i=0;i<b.length;i++){
-//     console.log(b[i]);
-// }
-
-// for in
-
-// for( var i in b){
-//     console.log(b[i])
-// }
-
-// for( var i of b){
-//     console.log(i)
-// }
-
-// for(var key in a){
-//     console.log(key)
-// }
-
-// for (var key in a) {
-//     console.log(key, " :", a[key])
-// }
-
-// console.log(Object.keys(a)); // []
-// console.log(Object.values(a)); // []
-
-// var a;
-// var a = 10;
-// a = 20;
-
-// console.log(a);
-
-// let b;
-// let b = 15;
-// b = 20;
-
-// console.log(b);
-
-// const c = 10;
-// c = 15;
-
-// console.log(c);
-
-// function run(){ // Create function
-//     console.log("Run")
-// }
-
-// run(); // Function Call 
-// run(); // Function Call 
-// run(); // Function Call 
-// run(); // Function Call 
-
-// const a = function run(){
-//     console.log("A function");
-// }
-
-// console.log(a());
-
-// var a = 15; // globle 
-
-// function run(){
-
-//     var b = 15; // function
-// // block
+//     var b =20;
+//     let c = 15;
 //     {
-//         var a = 10; // function
-//         const b = 20; // block
-//         let c = 20; // block
-
-//         console.log(b);
+//         var d = 50;
+//         const f = 50;
+//         let e = 50;
 //     }
-
-//     console.log(a);
-//     console.log(b);
 // }
 
-// // console.log(b); //error
+// var fname = "Om"
+// var lname = "Radadiya"
 
-// run();
-// console.log(a);
-
-
-// console.log(a);
-// var a = 15;
-
-// console.log(b);
-// let b = 20;
-
-function run(){
-    console.log("Hello");
-
-    return 20; // String
+const obj = {
+    fname: "Dhruv",
+    lname: "Suhagiya",
+    fullname : function fullname() { // parameter
+        console.log(this);
+        return `${this.fname} ${this.lname}`;
+    }
 }
 
-// var a = run();
+// key : value
 
-console.log(run());
+// const fullname = a.fname.concat(a.lname); // Dhruv Suhagiya
+// const fullname1 = `${a.fname} ${a.lname}` // Dhruv Suhagiya
+
+// function fullname(a, b) { // parameter
+//     return `${a} ${b}`;
+// }
+
+const value = obj.fullname(); // Call the function
+console.log(value);
+
+const fun = obj.fullname;
+const value2 = fun();
+
+console.log(value2);
+
+
+function sum(a,b){
+    return a+b;
+} 
+
+const multi = (a,b) => {
+    return a*b
+}
+
+console.log(sum(15,20));
+console.log(multi(15,20));
