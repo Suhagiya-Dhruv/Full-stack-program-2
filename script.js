57c519b43667f77c29eb1e864052b9598a95a20a
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-02-12 15:15:04 +0530

    Rating System

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-// querySelector
+const div = document.querySelector(".box-parent")
+const child = div.children;
 
-// const a = document.getElementById("3") // unique
-// console.log(a)
+for (let i = 0; i < child.length; i++) {
+    child[i].addEventListener('mouseenter', mouseEnter)
+    child[i].addEventListener('mouseleave', mouseLeave)
+}
 
-const a = document.getElementsByTagName("h1") // unique
-console.log(a)
+function mouseEnter(event) {
+    const value = Number(event.target.dataset.id)
+    colorFill(value, true)
+}
+
+function mouseLeave(event) {
+    const value = Number(event.target.dataset.id)
+    colorFill(value, false)
+}
+
+function colorFill(value, fill) {
+    if (fill) {
+        for (let i = 0; i < value; i++) {
+            child[i].style.backgroundColor = "yellow";
+        }
+    } else {
+        for (let i = 0; i < value; i++) {
+            child[i].style.backgroundColor = "transparent";
+        }
+    }
+}
