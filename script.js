3ddd232239baac037840c214daece66db5b24c32
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-02-15 15:05:58 +0530

    Rating system

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const div = document.querySelector(".box-parent")
 const child = div.children;
 
-for (let i = 0; i < child.length; i++) {
-    child[i].addEventListener('mouseenter', mouseEnter)
-    child[i].addEventListener('mouseleave', mouseLeave)
+let prvId = 0;
+div.addEventListener('click', function (e) {
+    const id = e.target.dataset.id;
+    if (id) {
+        prvId = id;
+        colorFill(id)
+    }
+})
+
+div.addEventListener('mouseleave', function(){
+    clearHoverColor();
+    colorFill(prvId)
+})
+
+for (var i = 0; i < child.length; i++) {
+    child[i].addEventListener("mouseenter", mouseEnter)
 }
 
 function mouseEnter(event) {
-    const value = Number(event.target.dataset.id)
-    colorFill(value, true)
+    clearHoverColor();
+    for (var i = 0; i < event.target.dataset.id; i++) {
+        child[i].classList.add("yellow")
+    }
+ 
 }
 
-function mouseLeave(event) {
-    const value = Number(event.target.dataset.id)
-    colorFill(value, false)
+function clearHoverColor() {
+    for (var i = 0; i < child.length; i++) {
+        child[i].classList.remove("yellow");
+        child[i].classList.remove("green");
+    }
 }
 
-function colorFill(value, fill) {
-    if (fill) {
-        for (let i = 0; i < value; i++) {
-            child[i].style.backgroundColor = "yellow";
-        }
-    } else {
-        for (let i = 0; i < value; i++) {
-            child[i].style.backgroundColor = "transparent";
-        }
+function colorFill(id) {
+    for (var i = 0; i < id; i++) {
+        child[i].classList.add("green");
     }
 }
